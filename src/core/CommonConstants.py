bef590c8d5771167fd077db0fb00e2d02f9a78b0
Ivan B <fhill122@gmail.com>
2015-09-11 20:48:38 +1000

    update

--- src/core/CommonConstants.py
+++ src/core/CommonConstants.py
@@ -1,3 +1,4 @@
 #~ here lists the common constants that are shared over all modules
-TableZ =-0.1 (tomesasure) #table relative z position to base
-GriperLength = 0.144
+
+## to do,  how to let python include file that not in either path or current folder
+
---
+++ src/vision/src/object_detector.h
@@ -0,0 +1,44 @@
+using namespace std;
+using namespace cv;
+
+class DesktopObject {
+    string name;
+    double height;
+    double size;
+    vector<Mat> images;
+    vector<float> matchDistance;
+    vector<int> matchNumber;
+    
+    public:
+        DesktopObject(string n,double h,double s){
+            name=n;
+            height=h;
+            size=s;
+        }
+        string getName(){return name;}
+        void addImages(Mat image,float distance, int number){
+            images.push_back(image);
+            matchDistance.push_back(distance);
+            matchNumber.push_back(number);
+        }
+        vector<Mat> & getImages(){
+            return images;
+        }
+        float getMatchDistance(int i){ return matchDistance[i];}
+        int getMatchNumber(int i){return matchNumber[i];}
+        
+};
+
+/** Function Headers */
+int detectAndDisplay( Mat frame, Mat obj, float matchDistance, int matchNumber, vector<KeyPoint> keypoints_object, Mat descriptors_object,vision::platePosition::Response &res);
+void imageCallback(const sensor_msgs::ImageConstPtr& msg);//copy image from topic to global variable frame
+bool get_plate_position(vision::platePosition::Request &req, vision::platePosition::Response &res); //service function
+bool displayFrame(vision::platePosition::Request &req, vision::platePosition::Response &res);
+int SIFTfeatureCalculate(Mat &img, vector<KeyPoint> &keypoints,Mat &descriptors );
+Mat readCalibration(ifstream &file);
+template <typename T> string tostr(const T& t) { 
+   ostringstream os; 
+   os<<t; 
+   return os.str(); 
+} 
+
